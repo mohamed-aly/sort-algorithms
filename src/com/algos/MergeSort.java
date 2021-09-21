2bb0b972bbf9a164713fe78be5bf1bccbc95e45d
mohamed-aly <mali902016@gmail.com>
2021-09-21 14:52:39 +0200

    MergeSort refactored

--- src/com/algos/MergeSort.java
+++ src/com/algos/MergeSort.java
@@ -10,43 +10,28 @@ public class MergeSort {
 
         if (end - start < 2) return;
 
-        int mid = (start + end) / 2;
+        int mid = (end + start) / 2;
 
         mergeSort(array, start, mid);
         mergeSort(array, mid, end);
         merge(array, start, mid, end);
+
     }
 
     private static void merge(int[] array, int start, int mid, int end) {
-
-        if (array[mid - 1] <= array[mid]) {
-            return;
-        }
+        if (array[mid - 1] < array[mid]) return;
 
         int i = start;
         int j = mid;
         int tempIndex = 0;
 
-
         int[] tempArray = new int[end - start];
 
         while (i < mid && j < end) {
-            tempArray[tempIndex++] =
-                    array[i] <= array[j]
-                            ? array[i++]
-                            : array[j++];
+            tempArray[tempIndex++] = array[i] <= array[j] ? array[i++] : array[j++];
         }
 
-//copying the rest if i index
-        System.arraycopy(array, i,
-                array,
-                start + tempIndex,
-                mid - i);
-
-        System.arraycopy(tempArray,
-                0,
-                array,
-                start,
-                tempIndex);
+        System.arraycopy(array, i, array, start + tempIndex, mid - i);
+        System.arraycopy(tempArray, 0, array, start, tempIndex);
     }
 }
